215f82eec89edf56ce3a63030c7b6ac4bebebce5
jschild01 <111925241+jschild01@users.noreply.github.com>
2024-09-23 21:10:23 -0400

    filtered Q&A

--- data/QA_eda.py
+++ data/QA_eda.py
@@ -17,6 +17,20 @@ def has_subject_and_verb(sentence):
             has_verb = True
     return has_subject and has_verb
 
+def validate_keyword(keyword): # return true if valid word
+    doc = nlp(keyword)
+    
+    # Filter out words that are less than 4 characters
+    if len(keyword) < 4:
+        return False
+    
+    # Check if the part of speech is noun, proper noun, verb, or adjective
+    for token in doc:
+        if token.pos_ in ['NOUN', 'PROPN', 'VERB', 'ADJ']:
+            return True
+    
+    return False
+
 # Load the English model in spaCy
 nlp = spacy.load("en_core_web_sm")  # Make sure to install this model with 'python -m spacy download en_core_web_sm'
 
@@ -38,40 +52,65 @@ df = df[df['keyword_generated_question'].str.endswith('?')]
 df = df[df['keyphrase_generated_question'].str.endswith('?')]
 print(len(df))
 
-# Correct grammar mistakes in 'keyphrase_generated_question'
+# Replaces faulty questions without proper subj-verb with 'Faulty question'
 df['keyword_generated_question_val'] = df['keyword_generated_question'].apply(has_subject_and_verb)
 df['keyphrase_generated_question_val'] = df['keyphrase_generated_question'].apply(has_subject_and_verb)
+df.loc[~df['keyword_generated_question_val'], 'keyword_generated_question'] = 'Faulty question'
+df.loc[~df['keyphrase_generated_question_val'], 'keyphrase_generated_question'] = 'Faulty question'
 
 # Remove instances where both keyword and keyphrase generated questions are not valid
 df = df[df['keyword_generated_question_val'] | df['keyphrase_generated_question_val']]
 print(len(df))
 
-# Replaces faulty questions without proper subj-verb with 'Faulty question'
-df.loc[~df['keyword_generated_question_val'], 'keyword_generated_question'] = 'Faulty question'
-df.loc[~df['keyphrase_generated_question_val'], 'keyphrase_generated_question'] = 'Faulty question'
+# Replaces faulty keywords not a (subj, adj, verb, pronoun) with 'Faulty keyword'
+df['keyword_tfidf_val'] = df['keyword_tfidf'].apply(validate_keyword)
+df['keyphrase_keybert_val'] = df['keyphrase_keybert'].apply(validate_keyword)
+df.loc[~df['keyword_tfidf_val'], 'keyword_tfidf'] = 'Faulty keyword'
+df.loc[~df['keyphrase_keybert_val'], 'keyphrase_keybert'] = 'Faulty keyword'
+
+# Remove instances where both keyword and keyphrase are not valid
+df = df[df['keyword_tfidf_val'] | df['keyphrase_keybert_val']]
+print(len(df))
+
+# Remove keyword_tfidf_val and keyphrase_keybert_val columns
+df = df.drop(columns=['keyword_generated_question_val', 'keyphrase_generated_question_val', 'keyword_tfidf_val', 'keyphrase_keybert_val'])
+
+# extract, clean, organize keyword/tfidf to split up tfidf and keybert derived questions and answers
+df_keyword = df[(df['keyword_generated_question'] != 'Faulty question') & (df['keyword_tfidf'] != 'Faulty keyword')]
+df_keyword = df_keyword.drop(columns=['token_count', 'keyphrase_keybert', 'keyphrase_keybert_score', 
+                                      'instruction_prompt_keyphrase', 'keyphrase_generated_question'])
+
+df_keyword['source_of_keyword'] = 'tfidf'
+
+df_keyword = df_keyword.rename(columns={'keyword_tfidf': 'answer'})
+df_keyword = df_keyword.rename(columns={'keyword_tfidf_score': 'relevance_score'})
+df_keyword = df_keyword.rename(columns={'instruction_prompt_keyword': 'instruction_prompt'})
+df_keyword = df_keyword.rename(columns={'keyword_generated_question': 'generated_question'})
+
+
+# extract, clean, organize keyphrase/keybert to split up tfidf and keybert derived questions and answers
+df_keyphrase = df[(df['keyphrase_generated_question'] != 'Faulty question') & (df['keyphrase_keybert'] != 'Faulty keyword')]
+df_keyphrase = df_keyphrase.drop(columns=['token_count', 'keyword_tfidf', 'keyword_tfidf_score', 
+                                      'instruction_prompt_keyword', 'keyword_generated_question'])
+
+df_keyphrase['source_of_keyword'] = 'keybert'
+
+df_keyphrase = df_keyphrase.rename(columns={'keyphrase_keybert': 'answer'})
+df_keyphrase = df_keyphrase.rename(columns={'keyphrase_keybert_score': 'relevance_score'})
+df_keyphrase = df_keyphrase.rename(columns={'instruction_prompt_keyphrase': 'instruction_prompt'})
+df_keyphrase = df_keyphrase.rename(columns={'keyphrase_generated_question': 'generated_question'})
+
+# combine the two dataframes
+df = pd.concat([df_keyword, df_keyphrase])
+print(f'Combined df length: {len(df)}')
+
+
+# filter to the samples in which the relevance score is greater than 0.4
+df = df[df['relevance_score'] > 0.4]
+print(len(df))
 
 # Save the filtered data
 df.to_csv('afc_txtFiles_QA_filtered.csv', index=False)
-
-
-
-
-
-
-
-
-
-#%%
-
-# read csv file
-df_og = pd.read_csv('afc_txtFiles_QA_filtered.csv')
-df = df_og.copy()
-
-df.head(10)
-
-
-
-
 
 
 
