fcfde2b857497832c8cb8c5fe73b4e927a113268
jschild01 <111925241+jschild01@users.noreply.github.com>
2024-10-03 20:56:46 -0400

    Update main-marc-xl.py with llama 1b

--- src/main-marc-xl.py
+++ src/main-marc-xl.py
@@ -51,7 +51,8 @@ def test_rag_system(data_dir: str, query: str):
         text_retriever.print_sample_documents(num_samples=5)
 
         print("Initializing RAG Generator...")
-        model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
+        #model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
+        model_name = 'meta-llama/Llama-3.2-1B-Instruct'
         hf_token = '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' # huggingface key req'd for llama model
         qa_generator = RAGGenerator(model_name=model_name, hf_token=hf_token)
         rag_pipeline = RAGPipeline(text_retriever, qa_generator)
